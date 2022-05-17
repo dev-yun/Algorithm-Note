cd2d76f99d06a6ff54739455b91fb8747e29fc7d
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-05-17 14:32:34 +0900

    Create maxSubbarraySum.js

---
+++ Algorithm/javascript/SlidingWindow/maxSubbarraySum.js
@@ -0,0 +1,39 @@
+// accepts an array and number n
+
+// 1. ㅂㅣㅎㅛㅇㅠㄹㅈㅓㄱ
+//   ㅁㅐㅂㅓㄴ ㅅㅐㄹㅗㅇㅜㄴ ㅂㅐㅇㅕㄹ tempㄹㅡㄹ ㅁㅏㄴㄷㅡㄹㅇㅓㅇㅑㅎㅏㄴㄷㅏ.
+const maxSubbarraySum = (arr, n) => {
+  if (arr.length < n) return null;
+
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - n + 1; i++) {
+    let temp = 0;
+    for (let j = 0; j < n; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) max = temp;
+  }
+  return max;
+};
+
+// 2. ㅎㅛㅇㅠㄹㅈㅓㄱ
+//   tempㅇㅕㄱㅎㅏㄹㅇㅡㄹ ㅎㅏㄴㅡㄴ tempSumㅇㅡㄹ ㅁㅏㄴㄷㅡㄹㄱㅗ
+//   ㅇㅣㅈㅓㄴ ㅇㅛㅅㅜㄹㅡㄹ ㅃㅐㄱㅗ ㄷㅏㅇㅡㅁ ㅇㅛㅅㅗㄹㅡㄹ ㄷㅓㅎㅏㄴㅡㄴ ㅅㅣㄱㅇㅡ
+//   ㅅㅣㄱㅇㅡㄹㅗ ㅈㅣㄴㅎㅐㅇㅎㅏㄴㄷㅏ.
+const maxSubbarraySumUsingSlide = (arr, n) => {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  if (arr.length < n) return null;
+  for (let i = 0; i < n; i++) {
+    tempSum += arr[i];
+  }
+  maxSum = tempSum;
+  for (let i = 0; i < arr.length; i++) {
+    tempSum = tempSum - arr[i] + arr[n + i];
+    if (tempSum > maxSum) {
+      maxSum = tempSum;
+    }
+  }
+  return maxSum;
+};
