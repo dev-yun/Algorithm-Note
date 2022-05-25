7076175e6444d9fcb41ade1f5e9b168ce3d1ec39
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-05-25 17:15:58 +0900

    Update selectionSort.js

--- Algorithm/javascript/Sorting/selectionSort.js
+++ Algorithm/javascript/Sorting/selectionSort.js
@@ -20,3 +20,21 @@ const selectionSort = (arr) => {
   }
   return arr;
 };
+
+// [0,1,2,3,4,7,6,5] 같이 거의 정렬된 정렬에서
+// 자기가 최소값일때는 이를 굳이 스왑할 필요가 없다.
+// 이를 처리하면 코드의 효율을 높힐 수 있다.
+
+const selectionSortOptimize = (arr) => {
+  // 첫번째 요소부터 기준으로 잡기
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    if (i !== minIndex) swap(arr, i, minIndex);
+  }
+  return arr;
+};
