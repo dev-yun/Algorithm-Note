cb11238e96d09b50b1b9c853c5fb598494b0f9a3
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-06-04 17:44:15 +0900

    Update 12917.js

--- programmers/javascript/12917.js
+++ programmers/javascript/12917.js
@@ -1,11 +1,24 @@
+// function solution(s) {
+//   var answer = '';
+//   const sArr = s.split('');
+//   const Asciiarr = sArr.map((e) => e.charCodeAt());
+
+//   Asciiarr.sort((a, b) => b - a);
+
+//   answer = Asciiarr.map((e) => String.fromCharCode(e));
+
+//   return answer.join('');
+// }
+
+// 문자열 자체를 return -1, 1을 통해 정렬하는 방법도 있다.
+// 더 빠름
 function solution(s) {
-  var answer = '';
   const sArr = s.split('');
-  const Asciiarr = sArr.map((e) => e.charCodeAt());
 
-  Asciiarr.sort((a, b) => b - a);
+  sArr.sort((a, b) => {
+    if (a > b) return -1;
+    else return 1;
+  });
 
-  answer = Asciiarr.map((e) => String.fromCharCode(e));
-
-  return answer.join('');
+  return sArr.join('');
 }
