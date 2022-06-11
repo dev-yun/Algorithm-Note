56885f0b1bb8be4278d1a70df871bc5596811045
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-06-11 23:17:08 +0900

    Update 42885.js

--- programmers/javascript/42885.js
+++ programmers/javascript/42885.js
@@ -1,3 +1,5 @@
+// 효율성 탈락
+
 function solution(people, limit) {
   var answer = 0;
   people.sort((a, b) => a - b);
@@ -9,6 +11,28 @@ function solution(people, limit) {
     } else {
       people.pop();
       people.shift();
+      answer++;
+    }
+  }
+
+  return answer;
+}
+
+// 효율성 통과
+
+function solution(people, limit) {
+  var answer = 0;
+  let lightPerson = 0;
+  let heavyPerson = people.length - 1;
+  people.sort((a, b) => a - b);
+
+  while (lightPerson <= heavyPerson) {
+    if (people[lightPerson] + people[heavyPerson] > limit) {
+      heavyPerson--;
+      answer++;
+    } else {
+      heavyPerson--;
+      lightPerson++;
       answer++;
     }
   }
