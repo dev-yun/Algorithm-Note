c5cfae0e0655bb282763e58c688f44b04af05c79
신윤철 <yunpro2766@gmail.com>
2022-06-15 00:19:22 +0900

    Update containerWithMostWater.js

--- leetcode/containerWithMostWater.js
+++ leetcode/containerWithMostWater.js
@@ -5,7 +5,6 @@ var maxArea = function(height) {
   let maxWater = Math.min(height[startIdx], height[endIdx]) * (endIdx - startIdx);
 
   while(startIdx < endIdx){
-    // console.log(startIdx, endIdx, maxWater)
     if(height[startIdx] <= height[endIdx]){
       startIdx++;
       containWater = Math.min(height[startIdx], height[endIdx]) * (endIdx - startIdx);
@@ -19,3 +18,15 @@ var maxArea = function(height) {
 
   return maxWater  
 };
+
+
+
+// 처음에는 단순히 2중 for문을 사용하여 풀려고 했지만 시간복잡도로 틀렸다.
+// 
+
+// for(let i = 0; i < height.length; i++){
+//   for(let j = i+1; j < height.length; j++){
+//     containWater = Math.min(height[i], height[j]) * (j - i);
+//     maxWater = Math.max(containWater, maxWater)
+//   }
+// }
