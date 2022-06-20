39b97d1eb768bf7947145141b0bf627e5275ebff
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-06-20 22:55:01 +0900

    Update 70129.js

--- programmers/javascript/70129.js
+++ programmers/javascript/70129.js
@@ -1,3 +1,30 @@
+// 효율성 좋은 코드
+
+function solution(s) {
+  var answer = [];
+  let oneStr = '';
+  let removeZeroCnt = 0;
+  let changeCnt = 0;
+
+  while (s.length > 1) {
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === '1') {
+        oneStr += s[i];
+      }
+    }
+
+    removeZeroCnt += s.length - oneStr.length;
+    s = oneStr.length.toString(2);
+    changeCnt++;
+    oneStr = '';
+  }
+
+  answer = [changeCnt, removeZeroCnt];
+  return answer;
+}
+
+// 효율성 떨어지는 코드
+
 function solution(s) {
   var answer = [];
   let removeZeroCnt = 0;
