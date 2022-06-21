85c168b027fd106cfb80ea683181151483cb8bbc
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-06-21 18:22:44 +0900

    Update FurthestBuildingYouCanReach.js

--- leetcode/javascript/Furthest Building You Can Reach/FurthestBuildingYouCanReach.js
+++ leetcode/javascript/Furthest Building You Can Reach/FurthestBuildingYouCanReach.js
@@ -1,3 +1,13 @@
+// 빌딩들의 높이 heights array가 주어지고 bricks와 ladders가 주어질때
+// 현재 빌딩의 높이가 다음 빌딩의 높이보다 크거나 같을때 ladders와 bricks는 필요하지 않다.
+// 하지만 현재 빌딩의 높이가 다음 빌딩보다 작을때 나는 ladder을 사용하거나 (h[i+1] - h[i]) bricks을 사용할 수 있다.
+// ladder : 다음 빌딩으로 갈수있는 밧줄
+// bricks : 다음 빌딩으로 갈수있게 바닥에 쌓는 벽돌? 1개당 높이 1.
+
+// 주의점 : ladder을 사용할 것인지. bricks를 사용할 것인지.
+// 1. bricks를 다 사용해도 넘을 수 없는 경우
+// 2. bricks를 사용하는 것보다 ladders를 사용하는 것이 효율적인 경우.(뒤의 높이들을 알아야함)
+
 /**
  * @param {number[]} heights
  * @param {number} bricks
