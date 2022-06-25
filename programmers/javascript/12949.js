86dac51a53cf76c68b66bdf7354647d67e5cc104
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-06-25 15:07:23 +0900

    Update 12949.js

--- programmers/javascript/12949.js
+++ programmers/javascript/12949.js
@@ -4,17 +4,19 @@ function solution(arr1, arr2) {
     { length: arr1.length },
     () => new Array(arr2[0].length)
   );
-  const arr1RowLen = arr1.length;
-  const arr1ColLen = arr1[0].length;
-  const arr2RowLen = arr2.length;
+  const arr1ColAndArr2RowLen = arr1[0].length;
   const arr2ColLen = arr2[0].length;
 
-  arr1.forEach((arr, row1) => {
+  arr1.forEach((_, row1) => {
     for (let col2 = 0; col2 < arr2ColLen; col2++) {
       let matrixMul = 0;
-      for (let col1 = 0; col1 < arr1ColLen; col1++) {
+      for (
+        let arr1ColArr2Raw = 0;
+        arr1ColArr2Raw < arr1ColAndArr2RowLen;
+        arr1ColArr2Raw++
+      ) {
         //console.log(row1,col1,",",col1,col2, arr1[row1][col1], arr2[col1][col2])
-        matrixMul += arr1[row1][col1] * arr2[col1][col2];
+        matrixMul += arr1[row1][arr1ColArr2Raw] * arr2[arr1ColArr2Raw][col2];
       }
       answer[row1][col2] = matrixMul;
     }
