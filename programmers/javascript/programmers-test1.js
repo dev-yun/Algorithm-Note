36d00457c00a39ee5a245c73f2fa6bfd30ab562f
신윤철 <71367408+dev-yun@users.noreply.github.com>
2022-07-13 20:40:43 +0900

    Create programmers-test1.js

---
+++ programmers/javascript/programmers-test1.js
@@ -0,0 +1,30 @@
+// 두 정수 x, y에서 공통으로 나타나는 정수 (135, 521의 공통 : [1, 5])
+// 짝꿍이 존재하지 않으면 -1, 0으로만 구성됐다면 0
+// (만약 짝꿍이 3, 0 ,3 이라면 짝꿍으로 만들 수 있는 수 중 가장 큰 수 인 330 반환)
+
+function solution(X, Y) {
+  var answer = '';
+  const commonNums = [];
+  let xIndex = 0;
+  let yIndex = 0;
+  const xArr = X.toString()
+    .split('')
+    .sort((a, b) => b - a);
+  const yArr = Y.toString()
+    .split('')
+    .sort((a, b) => b - a);
+
+  while (xIndex < xArr.length && yIndex !== yArr.length) {
+    if (xArr[xIndex] > yArr[yIndex]) xIndex++;
+    else if (xArr[xIndex] < yArr[yIndex]) yIndex++;
+    else {
+      answer += xArr[xIndex];
+      xIndex++;
+      yIndex++;
+    }
+  }
+
+  if (!answer) return '-1';
+
+  return Number(answer) === 0 ? '0' : answer;
+}
